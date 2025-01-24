eb0812c66284bd21d961503cad1c368f659db337
Costru Traian <85198344+anticheatsdev@users.noreply.github.com>
2025-01-24 04:29:18 +0200

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,76 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import Photoshop from "./company/Photoshop.png";
+import illustrator from "./company/illustrator.png";
+import autocad from "./company/autocad.png";
+import Visual from "./company/Visual--studio.png";
+
+import carrent from "./carrent.png";
+import barca from "./barca.png";
+import Race from "./Race.png";
+import jobit from "./jobit.png";
+import House from "./House.png";
+import tripguide2 from "./tripguide2.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  Photoshop,
+  illustrator,
+  Visual,
+  autocad,
+  carrent,
+  barca,
+  Race,
+  House,
+  jobit,
+  tripguide2,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,382 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  illustrator,
+  Photoshop,
+  Visual,
+  autocad,
+  shopify,
+  carrent,
+  jobit,
+  tripguide2,
+  House,
+  barca,
+  threejs,
+  Race,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Full Stack Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+  {
+    title: "Digital Solutions Architect",
+    icon: web,
+  },
+  {
+    title: "Responsive Design Expert",
+    icon: creator,
+  },
+  {
+    title: "Creative Technologist",
+    icon: mobile,
+  },
+  {
+    title: "UI/UX Designer",
+    icon: backend,
+  },
+
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: Photoshop,
+  },
+  {
+    name: "MongoDB",
+    icon: illustrator,
+  },
+  {
+    name: "Three JS",
+    icon: autocad,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "HTML, CSS and JS Developer",
+    company_name: "CVACODE",
+    icon: Visual,
+    iconBg: "#383E56",
+    date: "Feb 27, 2022",
+    points: [
+      "As a junior front-end developer, I have gained hands-on experience creating web forms that collect user input and send the data to designated email addresses. This allowed me to develop practical skills in web form development.",
+      "I create responsive, intuitive web forms that work seamlessly across devices, enhancing user experience and ease of interaction.",
+      "My work as a junior front-end developer has provided me with the skills necessary to create responsive and intuitive web-forms that enhance user experience.",
+
+    ],
+  },
+  {
+    title: "Photoshop",
+    company_name: "My Photoshop Skills and Experience:",
+    icon: Photoshop,
+    iconBg: "#E6DEDD",
+    date: "April 2023",
+    points: [
+      "I have experience with advanced features like layers, masks, and special effects, enabling me to create detailed and customized designs.",
+      "I have experience with advanced features like layers, masks, and special effects, enabling me to create detailed and customized designs.",
+      "Photoshop is a key tool in my creative process, allowing me to turn ideas into impactful and professional visuals tailored to each project.",
+      "I can quickly adapt my style and techniques to meet project requirements, delivering high-quality results efficiently for both personal and professional work.",
+    ],
+  },
+  {
+    title: "Adobe Illustrator",
+    company_name: "Mastering Adobe Illustrator:",
+    icon: illustrator,
+    iconBg: "#383E56",
+    date: "March 2023",
+    points: [
+      "I am skilled in using Adobe Illustrator for vector-based design, including logo creation, illustrations, and icon design. I can craft clean, scalable graphics for both digital and print media.",
+      "I am proficient in using Illustrator’s advanced features like the Pen Tool, Pathfinder, and Artboards, allowing me to create complex designs with precision and flexibility.",
+      "Illustrator is essential for my creative projects, enabling me to produce detailed artwork and polished designs. I focus on delivering high-quality visuals that are visually engaging and professional.",
+    ],
+  },
+  {
+    title: "AutoCAD",
+    company_name: "My Expertise in AutoCAD: Precision, Tools, and Efficiency:",
+    icon: autocad,
+    iconBg: "#E6DEDD",
+    date: "Jan 2024 - Present",
+    points: [
+      "I am proficient in using AutoCAD for creating precise 2D and 3D drawings. I can design architectural layouts, technical schematics, and engineering plans with accuracy and attention to detail.",
+      "I am skilled in using AutoCAD’s advanced tools, such as layer management, dimensioning, and block creation, to produce detailed, professional-quality designs for various industries.",
+      "AutoCAD is an essential tool in my workflow, allowing me to produce designs that meet both functionality and aesthetic standards. I prioritize efficiency and precision in every project I work on.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "Successfully completed over 10 projects for clients in industries ranging from web development to design, ensuring exceptional quality and timely delivery.",
+    name: "Delivered Projects",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://i.pinimg.com/1200x/5d/8d/0c/5d8d0c24e1612d7b429961464f3b80ea.jpg",
+  },
+  {
+    testimonial:
+      "Achieved a 95% client satisfaction rate by delivering solutions that exceeded expectations and aligned with their business goals.",
+    name: "High Client Ratings",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://i.pinimg.com/1200x/e0/53/28/e05328625e4d41ebd734e2ffdc607a5e.jpg",
+  },
+  {
+    testimonial:
+      "Streamlined development workflows to reduce project turnaround times by 20%, delivering faster results without compromising quality.",
+    name: "Workflow Optimization",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://raw.githubusercontent.com/speedperformance/Icons/refs/heads/main/Clock%20back%20with%20circular%20arrow%20free%20vector%20icons%20designed%20by%20Freepik.jpg",
+  },
+
+  {
+    testimonial:
+      "Leveraged expertise in React, Node.js, and Three.js to create scalable and user-friendly web applications tailored to unique client needs.",
+    name: "Tech Specialist",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://i.pinimg.com/1200x/dd/68/a8/dd68a89d6162fb629a21efd14c8ff147.jpg",
+  },
+
+  {
+    testimonial:
+      "Applied advanced skills in Photoshop, Adobe Illustrator, and AutoCAD to deliver visually stunning and functional designs for various projects.",
+    name: "Design Excellence",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://i.pinimg.com/1200x/3a/95/ed/3a95ed4894f7e20a138d812e940adbbc.jpg",
+  },
+
+  {
+    testimonial:
+      "Utilized Figma to design intuitive and visually appealing user interfaces, ensuring seamless user experiences and efficient prototyping for web and mobile applications.",
+    name: "Figma Expertise",
+    designation: "Lead Designer",
+    company: "Acme Co",
+    image: "https://i.pinimg.com/1200x/5a/7a/09/5a7a09170e9273d6e60775d031daaf89.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "A responsive car website created with HTML, CSS, and JavaScript featuring scrolling animations, smooth transitions between sections, a mobile-first approach, and compatibility with all mobile devices.",
+    tags: [
+      {
+        name: "HTML",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Java-Script",
+        color: "green-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://anticheatsdev.github.io/Elecars/",
+  },
+  {
+    name: "Watches Website",
+    description:
+      "TempoModer is a responsive watches website that features smooth scrolling, dark and light modes, and compatibility with all mobile devices. Developed with the Mobile First methodology, it provides a consistent user experience across all devices.",
+    tags: [
+      {
+        name: "HTML",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Java-Script",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://anticheatsdev.github.io/_TempoModern_/",
+  },
+  {
+    name: "What is Chat-GPT4?",
+    description:
+      "Chat-GPT is an AI chatbot powered by GPT-4, providing human-like conversation. It's a valuable tool for businesses and individuals seeking to automate customer support or improve online communication.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide2,
+    source_code_link: "https://what-is-chat-gpt-4.vercel.app/",
+  },
+  {
+    name: "Racing Pulse",
+    description:
+      "A simple yet meaningful website that marks the beginning of my web development journey. While it's not overly complex, it reflects my growth, creativity, and passion for coding. One of my first books, it serves as a milestone in my portfolio.",
+    tags: [
+      {
+        name: "HTML",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Java-Script",
+        color: "green-text-gradient",
+      },
+    ],
+    image: Race,
+    source_code_link: "https://qethrgbrswtdhdsdtha.tiiny.site",
+  },
+  {
+    name: "Responsive Pirate-Themed Website",
+    description:
+      "A responsive pirate-themed website created with HTML, CSS, and JavaScript, featuring a sleek design, smooth transitions, intuitive navigation, and a mobile-first approach for compatibility across all devices.",
+    tags: [
+      {
+        name: "Java-Script",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "HTML",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: barca,
+    source_code_link: "https://scarlet-modesty-79.tiiny.site",
+  },
+  {
+    name: "Responsive Pirate-Themed Website",
+    description:
+      "Relaxing Steep Lake House is a responsive architecture portfolio website featuring a modern design, clean typography, and smooth navigation. Built with a focus on aesthetics and functionality, it provides an immersive user experience across all devices.",
+    tags: [
+      {
+        name: "Java-Script",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "HTML",
+        color: "green-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: House,
+    source_code_link: "https://arhitectute3242435.tiiny.site",
+  },
+
+
+];
+
+
+
+export { services, technologies, experiences, testimonials, projects };
+
